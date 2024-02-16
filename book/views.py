ee3124f5b0b73056517bb7208b36db6b2db66753
tanzid64 <tanzid@inbox.ru>
2024-02-17 00:49:08 +0600

    Book app updated

--- book/views.py
+++ book/views.py
@@ -1,33 +1,51 @@
 from django.shortcuts import render
+from rest_framework.parsers import MultiPartParser, FormParser
 from .serializers import AuthorSerializer, BookSerializer, BookListCreateSerializer
 from .models import Author, Book
-from rest_framework import viewsets, status, permissions, generics
+from rest_framework import viewsets, status, permissions, generics, filters
 from category.permissions import IsModOrPublisherOrUser
 from .permissions import CanManageBooks
-# Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
+
+
 class AuthorView(viewsets.ModelViewSet):
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
-    permission_classes = [ IsModOrPublisherOrUser]
+    permission_classes = [IsModOrPublisherOrUser]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['id']
+    search_fields = ['first_name', 'last_name', 'description']
 
 class BookView(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
     permission_classes = (CanManageBooks,)
+    parser_classes = (MultiPartParser, FormParser)
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['id', 'language', 'isbn', 'publication_date', 'category', 'author']
+    search_fields = ['title', 'isbn']
         
     def perform_create(self, serializer):
-        # Assigning the publisher to the logged-in user during book creation
-        serializer.save(publisher=self.request.user)
+        if 'cover' in self.request.data: 
+            image = self.request.data['cover']  
+            serializer.validated_data['cover'] = image
+        serializer.save(publisher=self.request.user.publisher)
 
     def perform_update(self, serializer):
-        # Assigning the publisher to the logged-in user during book update
-        serializer.save(publisher=self.request.user)
+        if 'cover' in self.request.data: 
+            image = self.request.data['cover']  
+            serializer.validated_data['cover'] = image
+        serializer.save(publisher=self.request.user.publisher)
 
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookListCreateSerializer
+    parser_classes = (MultiPartParser, FormParser)
 
     def perform_create(self, serializer):
-        serializer.save(publisher=self.request.user)
+        if 'cover' in self.request.data: 
+            image = self.request.data['cover']  
+            serializer.validated_data['cover'] = image
+        serializer.save(publisher=self.request.user.publisher)
 
 
