6183c34ca7f23ea4914f0ec654f62f0c3887550d
tanzid64 <tanzid@inbox.ru>
2024-02-25 01:06:50 +0600

    Default img added in account and book

--- Account/models.py
+++ Account/models.py
@@ -5,7 +5,7 @@ from .managers import UserManager
 
 # Create your models here.
 class User(TimeStampMixin, AbstractUser):
-    avater = models.ImageField(upload_to="media/profilePicture/", default="./static/default_avater.png")
+    avater = models.ImageField(upload_to='media/profile_picture/', default="./media/profile_picture/avater.jpg")
     phone = models.CharField(max_length=15, null=True)
     balance = models.DecimalField(decimal_places=2, max_digits=12, default=0)
     email = models.EmailField(unique=True)
