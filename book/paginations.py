7622711b456a6e216671cdcf493804afdbbc3d25
tanzid64 <tanzid@inbox.ru>
2024-02-25 02:57:49 +0600

    Total pages added

--- book/paginations.py
+++ book/paginations.py
@@ -1,8 +1,16 @@
 from rest_framework.pagination import PageNumberPagination
-
+from rest_framework.response import Response
 class BookPagination(PageNumberPagination):
     page_size = 1
     page_query_param = "p"
     page_size_query_param = "records"
     max_page_size = 40
-    last_page_strings = "last"
+    def get_paginated_response(self, data):
+        return Response({
+            'total_objects': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'current_page_number': self.page.number,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data,
+        })
